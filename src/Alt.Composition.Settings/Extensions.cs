b4d1eaa8c2fafcedaa0e5a7e7996e97f9a0563f5
nblumhardt <nblumhardt@nblumhardt.com>
2012-12-11 22:13:46 +1000

    Initial basic solution setup.

---
+++ src/Alt.Composition.Settings/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alt.Composition.Settings
+{
+    /// <summary>
+    /// Extension methods provided by this library.
+    /// </summary>
+    public static class Extensions
+    {
+    }
+}
